3961106b66fe06ca9583b3c26cf135112f5dd499
Michael Josef Beer <michael@ubeer.org>
2016-05-23 20:50:08 +0000

    UPDATE - Added autotagging script

---
+++ bin/retag_build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+LAST_BUILD=$(git tag -l "build-*" | tail -n 1 | sed -e "s/build-\(.*\)/\1/g")
+
+if [ -z "$LAST_BUILD" ]; then
+    LAST_BUILD=0
+fi
+echo $LAST_BUILD
+
+LAST_BUILD=$(expr $LAST_BUILD + 1)
+LAST_BUILD=$(printf "%06i" $LAST_BUILD)
+
+git tag "build-${LAST_BUILD}"
+
+echo $LAST_BUILD
