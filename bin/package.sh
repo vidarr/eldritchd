328c174bd1651687f8ecddf11fdc6db5631c9ab4
Michael Josef Beer <michael@ubeer.org>
2018-04-01 19:30:49 +0000

    Increased version to 1.1.0 - Debugged bin/package.sh

--- bin/package.sh
+++ bin/package.sh
@@ -1,18 +1,21 @@
 #!/usr/bin/bash
-PACKAGE_DIR=../eldritch
+PACKAGE_DIR=eldritch
+BIN_FILES_TO_PACKAGE="setup.sh uninstall.sh"
 
-cd ../release && make buildnum && make
+cd release && make buildnum && make
 cd -
 
 [ -d $PACKAGE_DIR ] || mkdir $PACKAGE_DIR
 [ -d $PACKAGE_DIR/release ] || mkdir $PACKAGE_DIR/release
 [ -d $PACKAGE_DIR/release/src ] || mkdir $PACKAGE_DIR/release/src
+[ -d $PACKAGE_DIR/bin ] || mkdir $PACKAGE_DIR/bin
 
-cd $PACKAGE_DIR
-cp ../release/src/eldritchd release/src
+cp release/src/eldritchd ${PACKAGE_DIR}/release/src
 
-cp ../etc . -r
-cp ../bin . -r
+cp etc ${PACKAGE_DIR} -r
 
-tar cf ../eldritch.tar.gz $PACKAGE_DIR
+for f in $BIN_FILES_TO_PACKAGE; do
+    cp bin/$f ${PACKAGE_DIR}/bin
+done
 
+tar cf eldritch.tar.gz $PACKAGE_DIR
